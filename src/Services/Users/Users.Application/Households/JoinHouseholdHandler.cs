77ac8e0584d51c612b9a434331a6825fe1dacf7d
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-15 01:24:30 +0200

    Add househouse Handlers

---
+++ src/Services/Users/Users.Application/Households/JoinHouseholdHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Application.Abstractions;
+using Users.Domain.Entities;
+using Users.Domain.Exceptions;
+using Users.Domain.ValueObjects;
+
+namespace Users.Application.Households
+{
+    public class JoinHouseholdHandler
+    {
+        private readonly IHouseholdRepository _householdRepository;
+
+        public JoinHouseholdHandler(
+
+            IHouseholdRepository householdRepository)
+        {
+            _householdRepository = householdRepository;
+        }
+        public async Task<JoinHouseholdResult> ExecuteAsync(string rawInviteCode, Guid userId)
+        {
+            InviteCode inviteCode = InviteCode.From(rawInviteCode);
+            var household = await _householdRepository.GetByInviteCodeAsync(inviteCode);
+            if(household == null)
+            {
+                throw new HouseHoldNotFoundException();
+            }
+            household.AddMember(userId);
+            await _householdRepository.UpdateAsync(household);
+            return new JoinHouseholdResult(household.Id, household.Name, household.InviteCode.Value, household.Members.Count);
+        }
+    }
+    public sealed record JoinHouseholdResult(Guid HouseholdId, string Name, string InviteCode, int MemberCount);
+}
