f6c27b27d361031429f0e4ec04f98fdcdb9797e9
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-15 02:11:24 +0200

    update to .net10

---
+++ src/Services/Users/Users.Infrastructure/UsersDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Domain.Entities;
+
+namespace Users.Infrastructure
+{
+    public class UsersDbContext: DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base( options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Household> Households { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("users");
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UsersDbContext).Assembly);
+        }
+    }
+}
