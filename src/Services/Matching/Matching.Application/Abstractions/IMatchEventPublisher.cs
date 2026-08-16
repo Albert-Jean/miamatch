b1b92105bf0951e6a4f8128f867a5ab7ff92f2ee
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-17 01:40:07 +0200

    Add matching application

---
+++ src/Services/Matching/Matching.Application/Abstractions/IMatchEventPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matching.Domain.Entities;
+
+namespace Matching.Application.Abstractions
+{
+    public interface IMatchEventPublisher
+    {
+        Task PublishMatchCreatedAsync(Match match);
+    }
+}
--- src/Services/Matching/Matching.Application/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Matching.Application;
-
-public class Class1
-{
-
-}
