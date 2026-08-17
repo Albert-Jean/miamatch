c14d2488bdd17a6feb59df49f9c22e04a81ba405
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-18 01:05:09 +0200

    Add Recipes.Application

---
+++ src/Services/Recipes/Recipes.Application/Abstractions/IDeckRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Recipe.Domain.Entities;
+
+namespace Recipes.Application.Abstractions
+{
+    public interface IDeckRepository
+    {
+        public Task<IReadOnlyCollection<Recipes.Domain.Entities.Recipe>> FetchRecipesAsync(int minimumCount);
+        public Task AddASync(Deck deck);
+        public Task<Deck?> GetMostRecentAsync(Guid householdId);
+    }
+}
---
+++ src/Services/Recipes/Recipes.Application/Abstractions/IRecipeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Recipes.Domain.Entities;
+
+namespace Recipes.Application.Abstractions
+{
+    public interface IRecipeRepository
+    {
+        public Task<IReadOnlyCollection<Recipes.Domain.Entities.Recipe>> GetAllAsync();
+        public Task AddRangeAsync(IEnumerable<Recipes.Domain.Entities.Recipe> recipes);
+        public Task<IReadOnlyCollection<Recipes.Domain.Entities.Recipe>> GetByIdsAsync(IEnumerable<Guid> ids);
+    }
+}
