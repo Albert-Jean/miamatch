36285b73f37405a9eaf769ea3202cb1c7c6c06cc
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-25 16:14:26 +0200

    add shoppingList.Application

---
+++ src/Services/ShoppingList/ShoppingList.Application/Abstractions/IRecipeClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.Application.Abstractions
+{
+    public interface IRecipeClient
+    {
+        Task<RecipeDetails?> GetRecipeAsync(Guid recipeId);
+    }
+
+    public sealed record RecipeDetails(Guid Id, string Name, IReadOnlyCollection<RecipeIngredientDto> Ingredients);
+    public sealed record RecipeIngredientDto(string Name, string Measure);
+}
---
+++ src/Services/ShoppingList/ShoppingList.Application/Abstractions/IShoppingListItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShoppingList.Domain.Entities;
+
+namespace ShoppingList.Application.Abstractions
+{
+    public interface IShoppingListItemRepository
+    {
+        Task<IReadOnlyCollection<ShoppingListItem>> GetForHouseholdAsync(Guid householdId);
+        Task AddAsync(ShoppingListItem item);
+    }
+}
