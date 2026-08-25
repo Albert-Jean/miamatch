bcf00f52e5f246f5883dbc2e113191224a3c8bc0
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-25 14:34:13 +0200

    Add Recipe.api

---
+++ src/Services/Recipes/Recipes.Api/Endpoints/RecipesEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using Recipes.Application.Decks;
+
+namespace Recipes.Api.Endpoints
+{
+    public static class RecipesEndpoints
+    {
+        public static void MapRecipesEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/decks/current", async (Guid householdId, ClaimsPrincipal user, GetOrGenerateDeckHandler handler) =>
+            {
+                var householdsIds = user.Claims.Where(c => c.Type == "householdId").Select(c => Guid.Parse(c.Value));
+                if (householdsIds.Contains(householdId))
+                {
+                    var result = await handler.ExecuteAsync(householdId);
+                    return Results.Ok(result);
+                }
+                else
+                {
+                    return Results.Forbid();
+                }
+            }).RequireAuthorization();
+        }
+    }
+}
