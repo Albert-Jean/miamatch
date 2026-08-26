c9190025afac45e833c2174b3cf65f0ea9b41338
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-26 11:53:00 +0200

    Add domain + application

---
+++ src/Services/Notifications/Notifications.Application/Abstractions/IPushNotificationSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Notifications.Domain.Entities;
+
+namespace Notifications.Application.Abstractions
+{
+    public interface IPushNotificationSender
+    {
+       Task SendAsync(PushSubscription subscription, string payload);
+    }
+}
---
+++ src/Services/Notifications/Notifications.Application/PushSubscriptions/RegisterPushSubscriptionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Notifications.Application.Abstractions;
+using Notifications.Domain.Entities;
+namespace Notifications.Application.PushSubscriptions
+{
+    public class RegisterPushSubscriptionHandler
+    {
+        private readonly IPushSubscriptionRepository _pushSubscriptionRepository;
+        public RegisterPushSubscriptionHandler(IPushSubscriptionRepository pushSubscriptionRepository)
+        {
+            _pushSubscriptionRepository= pushSubscriptionRepository;
+        }
+        public async Task ExecuteAsync(Guid userId,string endpoint,string p256dhKey, string authKey)
+        {
+            var pushSubscription = PushSubscription.Create(userId,endpoint,p256dhKey,authKey);
+            await _pushSubscriptionRepository.AddAsync(pushSubscription);
+        }
+    }
+}
