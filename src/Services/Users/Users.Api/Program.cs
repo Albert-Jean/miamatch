a8376d0ef2878db7d328a8ccf63a2a9abe24b655
PouletJuteux <jean.albert.pro@gmail.com>
2026-08-27 16:15:09 +0200

    deploy on aws --> lamba + RDS + SQS

--- src/Services/Users/Users.Api/Program.cs
+++ src/Services/Users/Users.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Amazon.Lambda.AspNetCoreServer.Hosting;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -13,7 +14,7 @@ using Users.Infrastructure.Persistence.Repositories;
 using Users.Infrastructure.Security;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
